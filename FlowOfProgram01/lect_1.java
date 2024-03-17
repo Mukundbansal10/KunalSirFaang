6382bcd89c3f86d7c5e47afb70e0756afebfb892
Mukund70 <mukundbansal10@gmail.com>
2024-03-17 12:13:55 +0530

    kunal sir questions

---
+++ FlowOfProgram01/lect_1.java
@@ -0,0 +1,162 @@
+package FlowOfProgram01;
+
+import java.util.Scanner;
+
+import javax.sound.midi.SysexMessage;
+import javax.swing.plaf.synth.SynthSliderUI;
+
+// Assignment- 1
+/**
+ * lect_1
+ */
+public class lect_1 {
+
+    public static void main(String[] args) {
+        
+
+        // #1= even or odd 
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the number:");
+        // int num = sc.nextInt();
+        // if(num%2==0){
+        //     System.out.println("even");
+        // }
+        // else{
+        //     System.out.println("odd");
+        // }
+
+
+        // #2= greeting
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your name :");
+        // String name = sc.nextLine();
+        // System.out.println("hello "+ name + " nice to meet you ");
+
+
+
+        // #3= find simple interest
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter principal,time and rate ");
+        // int principal = sc.nextInt();
+        // int time = sc.nextInt();
+        // int rate = sc.nextInt();
+        // float S_I = (principal*time*rate)/100;
+        // System.out.println(S_I);
+
+
+        // #4= simple calculator 
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter first number");
+        // int first_num = sc.nextInt();
+        // System.out.println("Enter Second number");
+        // int second_num = sc.nextInt();
+        // System.out.println("please follow given instructions ");
+        // System.out.println("enter A for +");
+        // System.out.println("enter S for -");
+        // System.out.println("enter M for *");
+        // System.out.println("enter D for /");
+        // System.out.println("now enter :");
+        // char operator = sc.next().charAt(0);
+        // if (operator=='+'){
+        //     System.out.println(first_num+second_num);
+        // }
+        // else if(operator=='-') {
+        //     System.out.println(first_num-second_num);
+        // }
+        // else if(operator=='*'){
+        //     System.out.println(first_num*second_num);
+        // }
+        // else if(operator=='/'){
+        //     if(second_num!=0){
+        //         System.out.println(first_num/second_num);
+        //     }
+        //     else{
+        //         System.out.println("ERROR: zero division error ");
+        //     }
+        //         }
+        // System.out.println("thanks you end of the program ");
+
+        
+
+        // #5= largest number 
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter first number :");
+        // int first_num = sc.nextInt();
+        // System.out.print("enter second number:");
+        // int second_num = sc.nextInt();
+        // if (first_num>second_num){
+        //     System.out.println(first_num);
+        // }
+        // else{
+        //     System.out.println(second_num);
+        // }
+
+
+
+        // #6= INR to USD 
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter currency in INR:");
+        // float inr = sc.nextFloat();
+        // float usd = inr / 83.26f;
+        // System.out.println(usd);
+
+
+        // #7= fibonacci series 
+
+        // Scanner sc = new Scanner(System.in);
+        // int num_1 = 0;
+        // int num_2 = 1;
+        // System.out.print("enter no of times you want to run:");
+        // int n_times = sc.nextInt();
+        // System.out.println(num_1);
+        // System.out.println(num_2);
+        // int sum ;
+        // for(int i=1;i<=n_times;i++){
+        //     sum= num_1+num_2;
+        //     System.out.println(""+sum);
+        //     num_1=num_2;
+        //     num_2=sum;
+        // }
+
+
+        // #8=reverse string
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter the string :");
+        // String str = sc.next();
+        // String reverse= "" ;
+        // for(int i = str.length()-1;i>=0;i--){
+        //     reverse+= str.charAt(i);
+        // }
+        // System.out.print("reverse string is :"+reverse);
+
+        
+
+        // #9= palindrome 
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter the string:");
+        // String str = sc.next();
+        // String reverse = "";
+        // for(int i = str.length()-1;i>=0;i--){
+        //     reverse+=str.charAt(i);
+        // }
+        // if(str.equals(reverse)){
+        //     System.out.println("string is palindrome");
+        // }
+        // else{
+        //     System.out.println("string is not palindrome");
+        // }
+
+
+        // Scanner sc = new Scanner(System.in);
+        
+        
+        }
+}
