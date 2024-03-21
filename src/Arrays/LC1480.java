6acb669b6995fc36fb09ed854b030811662c901c
Mukund70 <mukundbansal10@gmail.com>
2024-03-21 23:33:19 +0530

    solve qne 1480 leetcode

--- src/Arrays/LC1480.java
+++ src/Arrays/LC1480.java
@@ -15,7 +15,7 @@ public class LC1480 {
 }
 class Main1{
     public static void main(String[] args) {
-        int[] nums = new int[]{2,4,3,5,7};
+        int[] nums = new int[]{2,4,3,5,7,2,4,3,5,7};
         LC1480 lc1480 = new LC1480();
         int[] runningSum = lc1480.runningSum(nums);
         System.out.println("Arrays.toString(nums) = " + Arrays.toString(runningSum));
