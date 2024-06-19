be57aff327bf7a6016e3a70bb612b03026233c09
Mukund70 <mukundbansal10@gmail.com>
2024-06-19 21:59:29 +0530

    solve again using shifting approach

--- src/Arrays/LC1389.java
+++ src/Arrays/LC1389.java
@@ -8,13 +8,36 @@ public class LC1389 {
 
     public static void createArray(int[] nums, int[] index) {
 
-        List<Integer> targetList = new ArrayList<>();
+//        approach 1
+
+        int[] target = new int[nums.length];
+        int size = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            targetList.add(index[i], nums[i]);
+            int idx = index[i];
+            if (idx < size) {
+                // Shift elements to the right
+                System.arraycopy(target, idx, target, idx + 1, size - idx);
+            }
+            target[idx] = nums[i];
+            size++;
         }
-//        convert list to Array
-        int[] target = targetList.stream().mapToInt(Integer::intValue).toArray();
         System.out.println("Arrays.toString(target) = " + Arrays.toString(target));
+
+
+
+
+
+
+//        approach 2
+
+//        List<Integer> targetList = new ArrayList<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            targetList.add(index[i], nums[i]);
+//        }
+////        convert list to Array
+//        int[] target = targetList.stream().mapToInt(Integer::intValue).toArray();
+//        System.out.println("Arrays.toString(target) = " + Arrays.toString(target));
 
 
     }
@@ -25,5 +48,5 @@ public class LC1389 {
         int[] index = {0, 1, 2, 2, 1};
         createArray(nums, index);
 
-    }
-}
+    }}
+
