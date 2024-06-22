f9cb714593e61607e285d3b0c81539212849f88e
Mukund70 <mukundbansal10@gmail.com>
2024-06-22 12:26:14 +0530

    solve LC problem 867

---
+++ src/Arrays/LC867.java
@@ -0,0 +1,39 @@
+package src.Arrays;
+
+import com.sun.jdi.connect.Connector;
+
+import java.util.Arrays;
+
+public class LC867 {
+    public static void main(String[] args) {
+
+//        transpose Matrix
+        int [][]arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+//        output
+//        [[1,4,7],
+//        [2,5,8],
+//        [3,6,9]]
+//        so we can replace the opp elements except the diagonals one
+        transposeMatrix(arr);
+    }
+    public static void transposeMatrix(int[][]arr){
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = row+1; col < arr[row].length; col++) {
+                int temp = arr[row][col];
+                arr[row][col]= arr[col][row];
+                arr[col][row] = temp;
+            }
+        }
+        for(int[] a:arr){
+            System.out.println("Arrays.toString(a) = " + Arrays.toString(a));
+        }
+    }
+
+
+
+
+}
